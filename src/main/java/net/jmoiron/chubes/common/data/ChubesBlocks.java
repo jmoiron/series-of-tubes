b543ea40484fb5285b08e15c34fa566ea94aef3b
Jason Moiron <jmoiron@jmoiron.net>
2025-03-01 09:01:13 -0500

    fix the blockitem rendering for the cable so it is an isometric version of the model

--- src/main/java/net/jmoiron/chubes/common/data/ChubesBlocks.java
+++ src/main/java/net/jmoiron/chubes/common/data/ChubesBlocks.java
@@ -6,11 +6,15 @@ import com.tterrag.registrate.util.entry.BlockEntry;
 
 import net.jmoiron.chubes.common.blocks.ChubesCableBlock;
 import net.minecraft.tags.BlockTags;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 
 public class ChubesBlocks {
 
     public static final BlockEntry<ChubesCableBlock> ChubesCable = REGISTRATE
         .block("cable", ChubesCableBlock::new)
+        .initialProperties(() -> Blocks.IRON_BLOCK)
         .lang("cable")
         .blockstate((ctx, prov) -> ChubesModels.cableModel(ctx, prov))
         .tag(BlockTags.MINEABLE_WITH_PICKAXE)
