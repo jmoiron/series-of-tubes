0e33b16322fc2cb596f24e0211ccbc62a24bd531
Jason Moiron <jmoiron@jmoiron.net>
2025-03-05 13:01:28 -0500

    use a cutout model instead of trying to get transparency to work to kill off my z-fighting conundrum, which leaves some lines that might be fixable by plaster, but that is a fight for another day

--- push.sh
+++ push.sh
@@ -2,7 +2,21 @@
 
 VERSION="0.5.1"
 WINUSER="jlmoi"
-INSTALL_PATH="/mnt/c/Users/$WINUSER/AppData/Roaming/PrismLauncher/instances/quantum-skies-$VERSION/minecraft/mods/"
+
+WINPATH="/mnt/c/users/$WINUSER/AppData/Roaming"
+LINPATH="$HOME/.local/share"
+PACKPATH="instances/quantum-skies-$VERSION/minecraft/mods"
+
+if [ -d "$WINPATH/PrismLauncher" ]; then
+    INSTALL_PATH="$WINPATH/PrismLauncher/$PACKPATH"
+elif [ -d "$LINPATH/PrismLauncher" ]; then
+    INSTALL_PATH="$LINPATH/PrismLauncher/$PACKPATH"
+else
+    echo "Cannot find PrismLauncher path."
+    exit 1
+fi
+
+#INSTALL_PATH="/mnt/c/Users/$WINUSER/AppData/Roaming/PrismLauncher/instances/quantum-skies-$VERSION/minecraft/mods/"
 CHUBES_VERSION="0.0.1"
 
 cp "build/libs/chubes-$CHUBES_VERSION.jar" "$INSTALL_PATH"
